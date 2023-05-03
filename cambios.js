dddefbe9bf878f75ef8a7eebac98cd057a40cf80
bogalejandro <faqmac90@gmail.com>
2023-05-03 04:47:24 -0500

    agregarJS

--- cambios.js
+++ cambios.js
@@ -1,9 +1,18 @@
 function validarFormulario() {
-    var nombre = document.forms["formulario"]["nombre"].value;
-    var correo = document.forms["formulario"]["correo"].value;
-    var mensaje = document.forms["formulario"]["mensaje"].value;
-    if (nombre == "" || correo == "" || mensaje == "") {
-      alert("Por favor complete todos los campos.");
-      return false;
-    }
+  var nombre = document.forms["formulario"]["nombre"].value;
+  var correo = document.forms["formulario"]["correo"].value;
+  var mensaje = document.forms["formulario"]["mensaje"].value;
+  if (nombre == "" || correo == "" || mensaje == "") {
+    alert("Por favor complete todos los campos.");
+    return false;
   }
+}
+
+function confirmarEnvio() {
+  var respuesta = confirm("¿Está seguro de que desea enviar el formulario?");
+  if (respuesta == true) {
+    return true;
+  } else {
+    return false;
+  }
+}
